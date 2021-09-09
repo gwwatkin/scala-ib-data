c6726a2abc6028be3e390fd17ef525096398830d
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2021-09-09 12:34:27 -0700

    Set up symbol search

--- build.sbt
+++ build.sbt
@@ -2,17 +2,17 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
-//lazy val ibclient = RootProject(file("external/ibclient"))
+lazy val ibclient = RootProject(file("external/ibclient"))
 
-
-
-Compile/unmanagedSources := Seq( baseDirectory.value / "external/twsapi/IBJts/source/JavaClient")
-Compile/unmanagedJars += baseDirectory.value / "external/twsapi/IBJts/source/JavaClient/TwsApi.jar"
+// Use the official API directly
+// Compile/unmanagedSources := Seq( baseDirectory.value / "external/twsapi/IBJts/source/JavaClient")
+// Compile/unmanagedJars += baseDirectory.value / "external/twsapi/IBJts/source/JavaClient/TwsApi.jar"
 
 lazy val tradingdata2 = (project in file ("."))
   .settings(
     libraryDependencies := Seq("org.apache.spark" %% "spark-sql" % "3.1.2"),
   )
+  .dependsOn(ibclient)
 
 Compile/mainClass := Some("org.watkins.trading.Main")
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,66 @@
 package org.watkins.trading
 
-import com.ib.client.EClient
+import com.ib.client.Contract
+import com.ib.client.Types.{BarSize, DurationUnit, FundamentalType, SecType, WhatToShow}
+import com.ib.contracts.StkContract
+import com.larroy.ibclient.IBClient
 
-import scala.concurrent.{Await, Future}
+import java.util.{Calendar, Date}
 import scala.concurrent.duration._
-import scala.language.postfixOps
-import scala.util._
+import scala.concurrent.{Await, Future}
 import scala.io.StdIn._
+import scala.language.postfixOps
+
 
 
 object Main {
-  def main(args : Array[String]): Int =
+  def block[A](f: Future[A]): A = {
+    Await.result(f, Duration.Inf)
+  }
+
+  def main(args : Array[String]): Unit =
   {
-    println("Connecting to IB Gateway");
+    println("Connecting to IB Gateway")
+    val ibc = new IBClient("localhost", 4002, 1)
+    try {
+      block(ibc.connect())
+      if (!ibc.isConnected) {
+        println("Could not connect")
+        return
+      }
 
-    println("Requesting fundamental data");
+      // TODO find a contract for which I have a data subscription
+      println("Requesting data")
+      val symbol = new Contract()
+      symbol.symbol("AC")
+      symbol.exchange("TSE")
+      symbol.secType(SecType.STK.name)
+      symbol.currency("CAD")
+      import java.text.SimpleDateFormat
+      val ft = new SimpleDateFormat("yyyy-MM-dd")
 
-    while (true){
-      println("-->");
-      val input: String = readLine();
-      if (input == "q" || input == "quit")
-        return 0
-      else
-        println("Unknown command");
+      // val data = block(ibc.fundamentals(sAMD, FundamentalType.ReportSnapshot))
+      val data = block(ibc.historicalData(
+        symbol,
+        ft.parse("2021-09-07"),
+        1,
+        DurationUnit.DAY,
+        BarSize._1_min,
+        WhatToShow.TRADES));
+      println(data)
+
+      while (true) {
+        print("--> ")
+        val input: String = readLine()
+        if (input == "q" || input == "quit")
+          return
+        else
+          println("Unknown command")
+      }
+
     }
-
-    0;
+    finally {
+      ibc.disconnect();
+    }
   }
 }
